fec59925f5dc8b6b3b96e20d231e7ef46da00cc0
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-15 21:35:59 +0530

    1st View

--- myfirst/src/App.js
+++ myfirst/src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  render(){
+    return(
+      <div className="container">
+        <h2> REACT APP by Damindu </h2>
+      </div>
+    );
+  }
 }
-
 export default App;
