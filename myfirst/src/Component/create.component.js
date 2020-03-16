cc82a8a1dcf4e554625c94b6936df0168cfe73a2
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-03-16 17:59:12 +0530

    Create Form on Create Component

--- myfirst/src/Component/create.component.js
+++ myfirst/src/Component/create.component.js
@@ -3,8 +3,28 @@ import React, {Component} from 'react';
 export default class Create extends Component{
     render(){
         return(
-            <div>
-                <p> Welcome to Create Component</p>
+            <div style={{marginTop: 10}}>
+                <h3>Add New Business</h3>
+                <form>
+                    <div className="form-group">
+                        <label>Add Person Name : </label>
+                        <input type ="text" className="form-control"/>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add Business Name : </label>
+                        <input type ="text" className="form-control"/>
+                    </div>
+
+                    <div className="form-group">
+                        <label>Add NIC Number : </label>
+                        <input type ="text" className="form-control"/>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="submit" value="Register Business" className="btn btn-primary"/>
+                    </div>
+                </form>
             </div>
         )
         
